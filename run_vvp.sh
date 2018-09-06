f76ce8fa638660f442c8df837813d9f8571e9e76
mrichomme <morgan.richomme@orange.com>
2018-09-06 16:57:44 +0200

    Mv logs to /
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- run_vvp.sh
+++ run_vvp.sh
@@ -5,6 +5,7 @@ if [ "$(ls -A $1)" ]; then
    ls $1
    cd /src/valid_script/ice_validator/
    pytest /src/valid_script/ice_validator/tests --cache-clear --runxfail --template-dir=$1 --report-format=html
+   mv output /
 else
    echo "no Files found"
    exit 1
