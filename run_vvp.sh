3a1e11ad6b0e66db2719bb197bc163d46838b554
mrichomme <morgan.richomme@orange.com>
2018-09-12 17:01:01 +0200

    Exit with the exit code of pytest
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- run_vvp.sh
+++ run_vvp.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+ICE_VALIDATOR_PATH=/src/valid_script/ice_validator/
 echo "Run VVP"
 if [ "$(ls -A $1)" ]; then
    ls $1
-   cd /src/valid_script/ice_validator/
-   pytest /src/valid_script/ice_validator/tests --cache-clear --runxfail --template-dir=$1 --report-format=html
+   cd $ICE_VALIDATOR_PATH
+   pytest $ICE_VALIDATOR_PATH/tests --cache-clear --runxfail --template-dir=$1 --report-format=html
+   res=$?
    mv output /
+   exit $res
 else
    echo "no Files found"
    exit 1
