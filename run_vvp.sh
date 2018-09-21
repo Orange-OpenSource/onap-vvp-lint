0bcb1c9d800a8ef38520c03dfe067fcf8662ebed
mrichomme <morgan.richomme@orange.com>
2018-09-21 23:20:34 +0200

    do not run tests on empty directories
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- run_vvp.sh
+++ run_vvp.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-ICE_VALIDATOR_PATH=/src/valid_script/ice_validator/
+ICE_VALIDATOR_PATH=/src/valid_script/ice_validator
 echo "Run VVP"
 if [ "$(ls -A $1)" ]; then
-   ls $1
-   cd $ICE_VALIDATOR_PATH
-   pytest $ICE_VALIDATOR_PATH/tests --cache-clear --runxfail --template-dir=$1 --report-format=html
-   res=$?
-   mv output /
-   exit $res
+  count=`ls -1 *.env 2>/dev/null | wc -l`
+  # check there is at least an env file
+  if [ $count != 0 ]; then
+      ls $1
+      cd $ICE_VALIDATOR_PATH
+      pytest $ICE_VALIDATOR_PATH/tests --cache-clear --runxfail --template-dir=$1 --report-format=html
+      res=$?
+      mv $ICE_VALIDATOR_PATH/output/* /
+      exit $res
+  else
+    echo "Empty dir, skip"
 else
    echo "no Files found"
    exit 1
