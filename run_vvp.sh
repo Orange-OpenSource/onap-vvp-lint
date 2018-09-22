92d38cac75d0b5167fcecefaef244a59ec17f779
mrichomme <morgan.richomme@orange.com>
2018-09-22 15:25:22 +0200

    Add a status file if at least one linting is fail
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- run_vvp.sh
+++ run_vvp.sh
@@ -11,12 +11,16 @@ if [ "$(ls -A $1)" ]; then
       cd $ICE_VALIDATOR_PATH
       pytest $ICE_VALIDATOR_PATH/tests --cache-clear --runxfail --template-dir=$1 --html=/output/report.html
       res=$?
+      if [ $res -ne 0 ]; then
+          echo $res > /output/status
+      fi
       exit $res
   else
     echo "Empty dir, skip"
   fi
 else
    echo "no Files found"
+   echo "1" > /output/status
    exit 1
 fi
 
